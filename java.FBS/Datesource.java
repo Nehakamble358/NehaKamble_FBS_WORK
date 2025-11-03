248a195b4a7e48c01d8054b01e93e04f49a2c23d
Nehakamble358 <nehakambale781@gmail.com>
2025-11-03 13:59:17 +0530

    java

---
+++ java.FBS/Datesource.java
@@ -0,0 +1,16 @@
+class Date
+{
+	int day,month,year;
+}
+//Date class end here.
+class Test
+{
+	public static void main(String[]args){
+	Date d1;
+	d1=new Date();
+	System.out.println(d1);
+     }
+}
+
+o/p :- Date@7a81197d
+	
